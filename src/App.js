f237a9d88e66006b4182eeb42c78b135c0a7731d
Juan <jnvleite@gmail.com>
2020-02-21 15:13:10 -0300

    initializing project

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+import '~/config/ReactotronConfig';
+import Routes from '~/routes';
+import GlobalStyles from '~/styles/global';
+
+const App = () => (
+  <>
+    <Routes />
+    <GlobalStyles />
+  </>
+);
+
+export default App;
